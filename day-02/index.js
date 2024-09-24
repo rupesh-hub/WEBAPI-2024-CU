50d10579968945a801e5f2b6d66b4ed872fcd254
rupesh.dulal <dulalrupesh77@gmail.com>
2024-09-24 16:22:33 +0545

    feat: web api development: button click event

---
+++ day-02/index.js
@@ -0,0 +1,10 @@
+var text  = "This is new text";
+
+function changeText(){
+    let article = document.getElementById('article');
+    let temp = article.textContent;
+    article.textContent = text;
+    text = temp;
+    
+}
+
